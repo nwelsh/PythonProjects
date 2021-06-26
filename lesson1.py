93e3c25aee9a8c776ef5c7cb8249c3c4fb5dba35
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-06-26 16:04:36 -0500

    Create lesson1.py

---
+++ lesson1.py
@@ -0,0 +1,28 @@
+#Nicole Welsh Python Notes 
+#6/26/21 
+
+# variable practice/lists practice 
+numbers = []
+strings = []
+names = ["Nicole", "Paige", "Carter", "Ryan"]
+
+# find the second name in the list 
+second_name =  names[1]
+
+# add values to numbers list and strings list 
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+strings.append("Nicole")
+strings.append("Welsh")
+
+
+# Print arrays and element in the array 
+print(numbers)
+print(strings)
+# Use % to append, like C 
+print("The second name on the names list is %s" % second_name)
+
+# repeat a value by multiplying it 
+print("hello" * 10)
