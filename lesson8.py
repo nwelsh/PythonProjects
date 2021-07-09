d5769ddcf002a7f7a363e1c185d542022c053c8d
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-07-09 08:11:37 -0500

    Create lesson8.py

---
+++ lesson8.py
@@ -0,0 +1,14 @@
+# https://www.learnpython.org/en/Functions
+
+class MyClass:
+    printy = "Siring"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass()
+other = MyClass()
+
+other.printy = "hello"
+
+print(myobjectx.printy)
