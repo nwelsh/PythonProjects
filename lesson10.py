2370ee373f7aa36c14ebee38330ce63283bb0b80
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-07-24 08:57:41 -0500

    Update lesson10.py

--- lesson10.py
+++ lesson10.py
@@ -1,3 +1,4 @@
+# numpys and pandas 
 # https://www.learnpython.org/en/Numpy_Arrays
 # need to finish 
 
@@ -5,11 +6,13 @@
 height = [1.87,  1.87, 1.82, 1.91, 1.90, 1.85]
 weight = [81.65, 97.52, 95.25, 92.98, 86.18, 88.45]
 
-# Import the numpy package as np
-import numpy as np
+# lesson 11: https://www.learnpython.org/en/Pandas_Basics
 
-# Create 2 numpy arrays from height and weight
-np_height = np.array(height)
-np_weight = np.array(weight)
+dict = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
+       "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "Pretoria"],
+       "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+       "population": [200.4, 143.5, 1252, 1357, 52.98] }
 
-print(type(np_height))
+import pandas as pd
+brics = pd.DataFrame(dict)
+print(brics)
