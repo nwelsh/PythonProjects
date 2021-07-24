9ebf45940d2014f1ffebba1a11d97e077fb381af
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-07-24 08:58:24 -0500

    Update lesson10.py

--- lesson10.py
+++ lesson10.py
@@ -1,4 +1,4 @@
-# numpys and pandas 
+# numpys and pandas and random 
 # https://www.learnpython.org/en/Numpy_Arrays
 # need to finish 
 
@@ -16,3 +16,18 @@ dict = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
 import pandas as pd
 brics = pd.DataFrame(dict)
 print(brics)
+
+# https://www.learnpython.org/en/Generators
+
+import random
+
+def lottery():
+    # returns 6 numbers between 1 and 40
+    for i in range(6):
+        yield random.randint(1, 40)
+
+    # returns a 7th number between 1 and 15
+    yield random.randint(1,15)
+
+for random_number in lottery():
+       print("And the next number is... %d!" %(random_number))
