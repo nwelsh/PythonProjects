13dcdd0b0a83249d9713760e1e4b3fa24ed76f42
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-08-07 10:40:30 -0500

    Update lesson12.py

--- lesson12.py
+++ lesson12.py
@@ -7,3 +7,10 @@ for word in words:
           word_lengths.append(len(word))
 print(words) # print the words 
 print(word_lengths) # print their lengths
+
+# also does the same thing 
+sentence = "the quick brown fox jumps over the lazy dog"
+words = sentence.split()
+word_lengths = [len(word) for word in words if word != "the"]
+print(words)
+print(word_lengths)
