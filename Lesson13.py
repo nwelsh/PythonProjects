2096d9def3ec80425fbfbd72539310980f14a2c3
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-08-20 09:17:28 -0500

    Update Lesson13.py

--- Lesson13.py
+++ Lesson13.py
@@ -1,22 +1,22 @@
 # Lesson 13: https://www.learnpython.org/en/Multiple_Function_Arguments
 # August 20, 2021
 
-def foo(first, second, third, *therest):
-    print("First: %s" %(first))
-    print("Second: %s" %(second))
-    print("Third: %s" %(third))
-    print("And all the rest... %s" %(list(therest)))
+# edit the functions prototype and implementation
+def foo(a, b, c):
+    pass
 
-foo(1,2,3,4,5)
+def bar(a, b, c):
+    pass
 
-def bar(first, second, third, **options):
-    if options.get("action") == "sum":
-        print("The sum is: %d" %(first + second + third))
 
-    if options.get("number") == "first":
-        return first
-
-result = bar(1, 2, 3, action = "sum", number = "first")
-print("Result: %d" %(result))
+# test code
+if foo(1,2,3,4) == 1:
+    print("Good.")
+if foo(1,2,3,4,5) == 2:
+    print("Better.")
+if bar(1,2,3,magicnumber = 6) == False:
+    print("Great.")
+if bar(1,2,3,magicnumber = 7) == True:
+    print("Awesome!")
 
 
