4d847850c5ca6533716000e79382bec5f965c4c3
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-09-02 09:32:46 -0500

    Update practiceQuestions.py

--- practiceQuestions.py
+++ practiceQuestions.py
@@ -1,3 +1,9 @@
 # September 2, 2021
 
-# Describe the concept of scope in Python with reference to local and global scope: scope is wehre a variable is accessible. Local scope is within a python function, these can only be accessed within the function. Global scope is a variable in teh main part and these can be accessed throughout the program. Local can be made glocal by ising global keyword inside a function 
+# number 1
+# Describe the concept of scope in Python with reference to local and global scope: scope is wehre a variable is accessible. 
+# Like Java, Local scope is within a python function, these can only be accessed within the function. 
+# Global scope is a variable in the main part and these can be accessed throughout the program. 
+# Local can be made glocal by ising global keyword inside a function 
+
+
