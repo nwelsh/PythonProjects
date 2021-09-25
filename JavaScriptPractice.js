cf120960ebdf9d9bfa5417bd09c4e75fedb95ab0
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-09-25 13:53:44 -0500

    Update JavaScriptPractice.js

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -44,4 +44,23 @@ var obj1 = {
       
 // Collection of data as an ordered list
       
-var array1 = [5, "Hello", true, 4.1];   
+var array1 = [5, "Hello", true, 4.1];  
+
+//hoisting - behavior of js where all the variable and function declarations are moved to the top 
+hoistedVariable = 3;
+console.log(hoistedVariable); // outputs 3 even when the variable is declared after it is initialized	
+var hoistedVariable;
+
+hoistedFunction();  // Outputs " Hello world! " even when the function is declared after calling
+
+function hoistedFunction(){ 
+  console.log(" Hello world! ");
+}
+
+// == vs ===
+var x = 2;
+var y = "2";
+(x == y)  // Returns true since the value of both x and y is the same
+
+(x === y) // Returns false since the typeof x is "number" and typeof y is "string"
+
