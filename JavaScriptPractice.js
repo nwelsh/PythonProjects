b75f8f7d26acf1e2eea3596ee7f72a370309b788
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-09-25 13:48:12 -0500

    Create JavaScriptPractice.js

---
+++ JavaScriptPractice.js
@@ -0,0 +1,30 @@
+//data types 
+
+//string 
+var str = "Vivek Singh Bisht"; //using double quotes
+var str2 = 'John Doe'; //using single quotes
+
+//number 
+var x = 3; //without decimal
+var y = 3.6; //with decimal
+
+//big int 
+var bigInteger =  234567890123456789012345678901234567890;
+
+//boolean
+var a = 2;
+var b =  3;
+var c =  2;
+(a == b) // returns false
+(a == c) //returns true
+
+//undefined
+var x; // value of x is undefined
+var y = undefined; // we can also set the value of a variable as undefined
+
+//null
+var a = null;
+
+//symbol - store anonymous and unique values 
+var symbol1 = Symbol('symbol');
+
