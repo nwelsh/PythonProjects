20bb65b848f76b963ba012c44fad2d967aefaffe
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-09-26 10:02:32 -0500

    Update FrontEndPractice.js

--- FrontEndPractice.js
+++ FrontEndPractice.js
@@ -3,12 +3,109 @@
 //1:45-2:15 pm 9/27 CUNA
 //1:00-1:30 9/29 Singlewire
 
-//HTML 
+//*****************************************************************************************************************************************************************HTML 
 
-//
+//*****************************************************************************************************************************************************************Python
 
-//JS
+//*****************************************************************************************************************************************************************JS
 
-//Java 
+//*****************************************************************************************************************************************************************Java 
 
-//React 
+// source: https://www.interviewbit.com/java-interview-questions/
+
+/* Java focuses on code simplicity. java supports primitive data types - byte, boolean, char short, not object oriented */
+
+/* Instance vs local variables 
+instance: accessible by all methods in the class
+local: within a block, function, or constructor
+*/
+//instance 
+class Athlete {
+public String athleteName;
+public double athleteSpeed;
+public int athleteAge;
+}
+
+//local
+public void athlete() {
+String athleteName;
+double athleteSpeed;
+int athleteAge;
+}
+
+/* Data encapsulation: hiding data attributes and their behaviors in a single unit. setters and getters*/
+
+/* JIT: just in time, improving the performance during run time. it foes the task of complitiing parts of bye code having similar functionality at the same tieme and reduces the compile time */
+
+/* difference between equals() and ==
+equals: a method defined in obejct calss. used for checkin the eqality of conents between two objects 
+==: binary operator - checks for comparing addressed, checks memory 
+*/
+
+/* infinite loops  */
+for (;;)
+{
+   // Business logic
+   // Any break logic
+}
+
+while(true){
+  
+}
+do{
+   // Business logic
+   // Any break logic
+}while(true);
+/* constructor overloading - depending on number of parameters and their types, the constructors take in fifferent information*/
+
+class Hospital {
+int variable1, variable2;
+double variable3;
+public Hospital(int doctors, int nurses) {
+ variable1 = doctors;
+ variable2 = nurses;
+}
+public Hospital(int doctors) {
+ variable1 = doctors;
+}
+public Hospital(double salaries) {
+ variable3 = salaries
+}
+}
+
+
+/* mothod overloading vs overriding: overlaoding returns dif variables, overridingreturns same varibales. it extends. it has the same parameters  
+static methods CAN be overloaded
+static methods CANNOT be overridden
+*/
+
+
+/* final means it cannot change*/
+try {
+int variable = 5;
+}
+catch (Exception exception) {
+System.out.println("Exception occurred");
+}
+finally {
+System.out.println("Execution of finally block");
+}
+
+public static void main(String[] args) {
+String example = new String("InterviewBit");
+example = null;
+System.gc(); // Garbage collector called
+}
+public void finalize() { //called so the clean up activity is implemented
+// Finalize called
+} 
+/* The super keyword is used to access hidden fields and overridden methods or attributes of the parent class. */
+ public void foo(){
+           System.out.println("Parent class foo!");
+           super.foo();    //Calls foo method of Parent class inside the Overriden foo method of Child class.
+       }
+
+/* garbage collector: enure the memory resouce is used efficiently. Heap is cleaned up  */
+
+/* hash table: Hashtable stores key/value pairs in a hash table. When using a Hashtable, you specify an object that is used as a key, and the value that you want linked to that key.
+You use keys to access data */
