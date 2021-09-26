4751e8b4ef6501ae8a8eae5e119c556236b80db1
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-09-26 09:48:08 -0500

    Update javaPractice.java

--- javaPractice.java
+++ javaPractice.java
@@ -2,7 +2,23 @@
 
 /* Java focuses on code simplicity. java supports primitive data types - byte, boolean, char short, not object oriented */
 
-/* */
+/* Instance vs local variables 
+instance: accessible by all methods in the class
+local: within a block, function, or constructor
+*/
+//instance 
+class Athlete {
+public String athleteName;
+public double athleteSpeed;
+public int athleteAge;
+}
+
+//local
+public void athlete() {
+String athleteName;
+double athleteSpeed;
+int athleteAge;
+}
 
 /* */
 
