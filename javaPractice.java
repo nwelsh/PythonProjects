56ab50473f150ce0570ef319d57c70919c0c070f
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-09-26 09:56:37 -0500

    Update javaPractice.java

--- javaPractice.java
+++ javaPractice.java
@@ -24,17 +24,65 @@ int athleteAge;
 
 /* JIT: just in time, improving the performance during run time. it foes the task of complitiing parts of bye code having similar functionality at the same tieme and reduces the compile time */
 
-/* */
+/* difference between equals() and ==
+equals: a method defined in obejct calss. used for checkin the eqality of conents between two objects 
+==: binary operator - checks for comparing addressed, checks memory 
+*/
 
-/* */
+/* infinite loops  */
+for (;;)
+{
+   // Business logic
+   // Any break logic
+}
 
-/* */
+while(true){
+  
+}
+do{
+   // Business logic
+   // Any break logic
+}while(true);
+/* constructor overloading - depending on number of parameters and their types, the constructors take in fifferent information*/
 
-/* */
+class Hospital {
+int variable1, variable2;
+double variable3;
+public Hospital(int doctors, int nurses) {
+ variable1 = doctors;
+ variable2 = nurses;
+}
+public Hospital(int doctors) {
+ variable1 = doctors;
+}
+public Hospital(double salaries) {
+ variable3 = salaries
+}
+}
 
 
-/* */
+/* mothod overloading vs overriding: overlaoding returns dif variables, overridingreturns same varibales. it extends. it has the same parameters  */
 
+
+/* final means it cannot change*/
+try {
+int variable = 5;
+}
+catch (Exception exception) {
+System.out.println("Exception occurred");
+}
+finally {
+System.out.println("Execution of finally block");
+}
+
+public static void main(String[] args) {
+String example = new String("InterviewBit");
+example = null;
+System.gc(); // Garbage collector called
+}
+public void finalize() { //called so the clean up activity is implemented
+// Finalize called
+} 
 /* */
 
 /* */
