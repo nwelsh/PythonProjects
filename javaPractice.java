a026edca9e8f11713d6d5fc01415cbf6d8472b96
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-09-26 09:47:02 -0500

    Update javaPractice.java

--- javaPractice.java
+++ javaPractice.java
@@ -1,6 +1,6 @@
 // source: https://www.interviewbit.com/java-interview-questions/
 
-/* */
+/* Java focuses on code simplicity. java supports primitive data types - byte, boolean, char short, not object oriented */
 
 /* */
 
